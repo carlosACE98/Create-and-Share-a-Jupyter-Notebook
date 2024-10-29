3efe461f2957b5cffae8956c7b1c1fd4f700ba24
carlosACE98 <158324649+carlosACE98@users.noreply.github.com>
2024-10-28 20:06:47 -0600

    Add files via upload

---
+++ Data Science Tools and Ecosystem.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem.
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# __Objectives:__
+# 
+#     - Identify the different syntaxis in markdown.
+#     - Create tables in markdown.
+#     - Share your work on a github repo.
+
+# ### Some of the popular languages that Data Scientists use are:
+# 1. Python.
+# 2. R. 
+# 3. SQL.
+# 4. Scala
+
+# ### Some of the commonly used libraries used by Data Scientists include:
+# 1. NumPy.
+# 2. Matplotlib.
+# 3. Scikit-learn.
+# 4. PyTorch.
+
+# |Data Science Tools|
+# | ------------------------ |
+# | <center>GitHub</center>|
+# | <center>MySQL</center> |
+# | <center>Kibana</center> |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+(200/60)
+
+
+# ## Author
+# Carlos Alberto Calderón Espinosa
+
+# In[ ]:
+
+
+
+
